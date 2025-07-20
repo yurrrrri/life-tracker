4ea4368dcd33ff9ec9cc929517fb3fe7386b4a37
yurison <yu_ri_son@naver.com>
2025-07-20 10:13:23 +0900

    Revert "Style: shadcn-ui 추가"
    This reverts commit 9e8da775ef41c62e6e4cf0fe0c1d8a4095a2356e.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     port: 3000,
     open: true,
